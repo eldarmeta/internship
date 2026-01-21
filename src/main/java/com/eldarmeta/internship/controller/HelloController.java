45de30c4bd01ecfb93f78b38437f0fae36f56b0d
Eldar Abdivaitov <eldarmeta@outlook.com>
2026-01-21 00:46:57 -0500

    Initial Spring Boot project (Week 1)

---
+++ src/main/java/com/eldarmeta/internship/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.eldarmeta.internship.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Eldar backend started!";
+    }
+}
